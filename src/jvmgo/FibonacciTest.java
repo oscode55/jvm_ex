23d352847dad377af19629c51bd9a705d120553e
lijiecheng <1016030044@qq.com>
2018-11-07 09:12:44 +0800

    添加jvmgo所需要的java文件 invokestatic invokevirtual

---
+++ src/jvmgo/FibonacciTest.java
@@ -0,0 +1,18 @@
+package jvmgo;
+
+/**
+ * @Author: myname
+ * @Date: 2018/11/6 17:13
+ */
+public class FibonacciTest {
+    public static void main(String[] args) {
+        long x = fibonacci(30);
+        System.out.println(x);
+    }
+    private static long fibonacci(long n) {
+        if( n <= 1) {
+            return n;
+        }
+        return fibonacci(n-1) + fibonacci(n-2);
+    }
+}
---
+++ src/jvmgo/InvokeDemo.java
@@ -0,0 +1,29 @@
+package jvmgo;
+
+/**
+ * @Author: myname
+ * @Date: 2018/11/6 17:09
+ */
+public class InvokeDemo implements Runnable{
+    public static void main(String[] args) {
+        new InvokeDemo().test();
+    }
+    public void test(){
+        InvokeDemo.staticMethod(); //invokestatic
+        InvokeDemo demo = new InvokeDemo();
+        demo.instanceMethod(); //invokevirtual
+        super.equals(null);
+        this.run();
+        ((Runnable)demo).run();
+    }
+    public static void staticMethod(){
+
+    }
+    private void instanceMethod(){
+
+    }
+    @Override
+    public void run() {
+
+    }
+}
