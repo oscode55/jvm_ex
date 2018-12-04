30c85728589dd73a8aeda1d341835c29242ab145
lijiecheng <1016030044@qq.com>
2018-12-05 04:46:29 +0800

    jvmgo 第十章 异常处理

---
+++ src/exceptions/ExceptionApplication.java
@@ -0,0 +1,17 @@
+package exceptions;
+
+/**
+ * @Author: myname
+ * @Date: 2018/11/15 14:20
+ */
+public class ExceptionApplication {
+    public static void main(String[] args) {
+        //all extends
+        Class c0 = Throwable.class;
+        //Unckecked
+        Class c1 = RuntimeException.class;
+        Class c2 = Error.class;
+        //checked
+
+    }
+}
